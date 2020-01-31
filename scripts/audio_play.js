8c0389c701f391722753fe826449fc3c8b89772b
sprhere <59331304+sprhere@users.noreply.github.com>
2020-01-31 16:52:31 +0800

    Update All

---
+++ scripts/audio_play.js
@@ -0,0 +1,289 @@
+class audioRel {
+  constructor(name, txt) {
+    this.audio = "aud/" + name + ".mp3";
+    this.speaker = name.slice(12, name.indexOf("_", 12));
+    this.lyric = txt;
+  }
+}
+
+VO_337_M = new audioRel("VO_E1_S04_A_CHLOE_337_M", "(thinking) Taylor always reminds me of a vanilla-flavored mannequin that walks around in designer jeans judging everyone.");
+VO_338 = new audioRel("VO_E1_S04_A_TAYLOR_338", "Chloe! I was just thinking about you.");
+VO_339 = new audioRel("VO_E1_S04_A_CHLOE_339", "What?");
+VO_340 = new audioRel("VO_E1_S04_A_TAYLOR_340", "My mom and I were cleaning out some of my old clothes -- I volunteer at Goodwill -- and I was wondering if maybe you'd like me to bring some for you to try on?");
+VO_778 = new audioRel("VO_E1_S04_A_TAYLOR_778", "Maybe something a little less...");
+VO_779 = new audioRel("VO_E1_S04_A_TAYLOR_779", "Macabre?");
+VO_780 = new audioRel("VO_E1_S04_A_TAYLOR_780", "Edgar Allen Poe?");
+VO_781 = new audioRel("VO_E1_S04_A_TAYLOR_781", "Beer stained?");
+VO_341 = new audioRel("VO_E1_S04_A_CHLOE_341", "How nice of you, Taylor.");
+VO_342 = new audioRel("VO_E1_S04_A_TAYLOR_342", "I'm just looking out for you. We girls have to stick together.");
+VO_343 = new audioRel("VO_E1_S04_A_CHLOE_343", "Believe it or not, I'm actually able to dress myself. Thanks.");
+VO_344 = new audioRel("VO_E1_S04_A_TAYLOR_344", "Oh, I know! I just heard times are tough. I volunteer, you know.");
+VO_345 = new audioRel("VO_E1_S04_A_CHLOE_345", "You're a saint.");
+VO_346 = new audioRel("VO_E1_S04_A_TAYLOR_346", "By the way, what's the word on you and Rachel? Everyone's talking about that photo of the two of you, from last night? It's funny, I guess. I only say that because I had a party at my place last night, and Rachel didn't come.");
+VO_347 = new audioRel("VO_E1_S04_A_CHLOE_347", "Wow, that is so strange! I can't imagine why she wouldn't want to do that.");
+VO_348 = new audioRel("VO_E1_S04_A_TAYLOR_348", "Right? But I guess the two of you got up to some fun? I want to hear all about it!");
+VO_349 = new audioRel("VO_E1_S04_A_CHLOE_349", "I guess my invitation got lost in the mail?");
+VO_350 = new audioRel("VO_E1_S04_A_TAYLOR_350", "Totally! You also weren't there, and we were all really sad about that. But whatever, you were out having crazy fun! Where did you two go?");
+VO_351_M = new audioRel("VO_E1_S04_A_CHLOE_351_M", "(thinking) Insulting my clothing and then trolling me for information about Rachel? Do I really want to deal with this right now? Or should I make her smug face admit her friends act is bullshit?");
+VO_352 = new audioRel("VO_E1_S04_A_CHLOE_352", "You'll have to hear about it from someone else. I don't have the energy to talk to you right now.");
+VO_353 = new audioRel("VO_E1_S04_A_TAYLOR_353", "Oh my gosh, did you party literally all night? What amazing venue was this? Well, look, I'm gonna go let my mom know it's a definite yes on my old clothes, and then you can tell me all about last night's event while you're trying them on. See you, Chloe!");
+VO_354_M = new audioRel("VO_E1_S04_A_CHLOE_354_M", "(thinking) I hope she chokes on an ice cube.");
+VO_357_M = new audioRel("VO_E1_S04_A_CHLOE_357_M", "(thinking) Insulting my clothing and then trolling me for information about Rachel? I'm going to make her admit how fake she is.");
+VO_762 = new audioRel("VO_E1_S04_A_CHLOE_762", "Okay, cut the shit, Taylor. We're not friends, so stop acting like we are.");
+VO_356 = new audioRel("VO_E1_S04_A_TAYLOR_356", "What are you talking about? You and me, Chloe! Remember all the good times we had in freshman algebra?");
+VO_358 = new audioRel("VO_E1_S04_A_CHLOE_358", "Are you sure we had algebra together?");
+VO_359 = new audioRel("VO_E1_S04_A_TAYLOR_359", "Um... maybe I'm thinking of someone else?");
+VO_360 = new audioRel("VO_E1_S04_A_CHLOE_360", "Zero points for random guessing.");
+VO_361 = new audioRel("VO_E1_S04_A_CHLOE_361", "I think you mean the good times you gave Mr. Terry with your boobs hanging out in the front row. I sat in the back, remember?");
+VO_363 = new audioRel("VO_E1_S04_A_TAYLOR_363", "Well... even so... I think we should be better friends. I'm sure we have a lot in common.");
+VO_362 = new audioRel("VO_E1_S04_A_CHLOE_362", "What are you talking about? We're completely different.");
+VO_364 = new audioRel("VO_E1_S04_A_TAYLOR_364", "No, Chloe, we might seem different, but deep down we're the same.");
+VO_365 = new audioRel("VO_E1_S04_A_CHLOE_365", "Not really. I know we're both big users, but I've never used people.");
+VO_366 = new audioRel("VO_E1_S04_A_CHLOE_366", "Quit blowing sunshine up my ass. It's dark in there for a reason.");
+VO_368 = new audioRel("VO_E1_S04_A_TAYLOR_368", "I'm just trying to be nice. You don't have to bite my head off.");
+VO_367 = new audioRel("VO_E1_S04_A_CHLOE_367", "You're right. We should go clubbing sometime. I can do blow while you're doing blowjobs.");
+VO_369 = new audioRel("VO_E1_S04_A_TAYLOR_369", "You have such a unique sense of humor! So, like, biting and sarcastic. I just love that!");
+VO_370 = new audioRel("VO_E1_S04_A_CHLOE_370", "Ugh, look. I heard this fake-nice spiel from Victoria already. Now you? Coordinate your strategies, skanks.");
+VO_372 = new audioRel("VO_E1_S04_A_CHLOE_372", "Why are your teeth so fucking straight? Are they implants? Did someone punch you in the mouth for telling lies?");
+VO_373 = new audioRel("VO_E1_S04_A_CHLOE_373", "This conversation's making me feel even more pathetic than usual.");
+VO_374 = new audioRel("VO_E1_S04_A_TAYLOR_374", "You're angry because two different people were nice to you today?");
+VO_371 = new audioRel("VO_E1_S04_A_CHLOE_371", "Taylor? I mean this sincerely: I hope you go camping this weekend, I hope it's perfect weather, and I hope a bear eats you.");
+VO_375 = new audioRel("VO_E1_S04_A_TAYLOR_375", "You are such an unhappy person.");
+VO_379 = new audioRel("VO_E1_S04_A_CHLOE_379", "Are you sure I'm unhappy? 'Cause watching you grovel for Rachel Amber's attention is making me feel pretty good.");
+VO_376 = new audioRel("VO_E1_S04_A_TAYLOR_376", "Chloe, being unhappy is a state of mind that you control. Dr. Phil said that.");
+VO_377 = new audioRel("VO_E1_S04_A_CHLOE_377", "What the hell? You are, like, completely disconnected from reality.");
+VO_378 = new audioRel("VO_E1_S04_A_CHLOE_378", "Save your pity. You'll need it when Victoria finds someone else to wipe her ass.");
+VO_380 = new audioRel("VO_E1_S04_A_TAYLOR_380", "Fine, you win. I don't want to be friends with you. I'd never be your friend.");
+VO_381 = new audioRel("VO_E1_S04_A_TAYLOR_381", "Are you happy now?");
+VO_382_M = new audioRel("VO_E1_S04_A_CHLOE_382_M", "(thinking) Geez, I really hit a nerve there.");
+VO_383 = new audioRel("VO_E1_S04_A_TAYLOR_383", "I am always telling people how much personality you have. Anyway, I'll let my mom know it's a definite yes on my old clothes. See you, Chloe!");
+VO_384_M = new audioRel("VO_E1_S04_A_CHLOE_384_M", "(thinking) What the fuck?");
+VO_385_M = new audioRel("VO_E1_S04_A_CHLOE_385_M", "(thinking) This makes being high sound like a bad thing.");
+VO_355 = new audioRel("VO_E1_S04_A_CHLOE_355", "Okay, cut the shit, Taylor. We're not friends, so stop acting like we are.");
+
+
+var myTally = 0;
+var myInterval;
+var myInterval1;
+var myInterval2;
+var myInterval3;
+var myInterval4;
+
+var piece01 = {butt:"Wore Skeleton Butterfly shirt.", vo:[VO_337_M, VO_338, VO_339, VO_340, VO_778, VO_779], nextPiece:["piece04", "piece05", "", ""], score:0};
+var piece02 = {butt:"Wore Raven shirt.", vo:[VO_337_M, VO_338, VO_339, VO_340, VO_778, VO_780], nextPiece:["piece04", "piece05", "", ""], score:0};
+var piece03 = {butt:"Wore Firewalk shirt.", vo:[VO_337_M, VO_338, VO_339, VO_340, VO_778, VO_781], nextPiece:["piece04", "piece05", "", ""], score:0};
+
+var piece04 = {butt:"How nice of you.", vo:[VO_341, VO_342, VO_345, VO_346], nextPiece:["piece06", "piece07", "", ""], score:0};
+var piece05 = {butt:"I can dress myself.", vo:[VO_343, VO_344, VO_345, VO_346], nextPiece:["piece06", "piece07", "", ""], score:0};
+
+var piece06 = {butt:"Can't imagine why.", vo:[VO_347, VO_348], nextPiece:["piece08", "piece09", "", ""], score:0};
+var piece07 = {butt:"Guess my invitation got lost.", vo:[VO_347, VO_348], nextPiece:["piece08", "piece09", "", ""], score:0};
+
+var piece08 = {butt:"Walk away.(don't backtalk)", vo:[VO_351_M, VO_352, VO_353, VO_354_M], nextPiece:["", "", "", ""], score:0};
+var piece09 = {butt:"Call her out.(backtalk)", vo:[VO_357_M, VO_762, VO_356, VO_358, VO_359], nextPiece:["piece10", "piece11", "piece12", ""], score:0};
+
+var piece10 = {butt:"No partial credit.", vo:[VO_360, VO_363], nextPiece:["piece13", "piece14", "piece15", ""], score:4};
+var piece11 = {butt:"Textbook liar.", vo:[VO_361, VO_363], nextPiece:["piece13", "piece14", "piece15", ""], score:4};
+var piece12 = {butt:"Completely different.", vo:[VO_362, VO_364], nextPiece:["piece13", "piece14", "piece15", ""], score:1};
+
+var piece13 = {butt:"Two kinds of users.", vo:[VO_365, VO_368], nextPiece:["piece16", "piece17", "piece18", "piece19"], score:4};
+var piece14 = {butt:"Quit blowing sunshine.", vo:[VO_366, VO_369], nextPiece:["piece16", "piece17", "piece18", "piece19"], score:1};
+var piece15 = {butt:"Similar interests.", vo:[VO_367, VO_369], nextPiece:["piece16", "piece17", "piece18", "piece19"], score:1};
+
+var piece16 = {butt:"First Victoria, now you?", vo:[VO_370, VO_374], nextPiece:["piece20", "piece21", "piece22", ""], score:4};
+var piece17 = {butt:"Hope a bear eats you.", vo:[VO_371, VO_375], nextPiece:["piece20", "piece21", "piece22", ""], score:4};
+var piece18 = {butt:"Your weirdly perfect teeth.", vo:[VO_372, VO_375], nextPiece:["piece20", "piece21", "piece22", ""], score:4};
+var piece19 = {butt:"Pathetic conversation.", vo:[VO_373, VO_376], nextPiece:["piece20", "piece21", "piece22", ""], score:1};
+
+var piece20 = {butt:"Do you hear yourself?", vo:[VO_377], nextPiece:["", "", "", ""], score:1};
+var piece21 = {butt:"Sunny days always end.", vo:[VO_378], nextPiece:["", "", "", ""], score:4};
+var piece22 = {butt:"Sunny days always end.", vo:[VO_379], nextPiece:["", "", "", ""], score:4};
+
+var piece23 = {butt:"", vo:[VO_380, VO_381, VO_382_M], nextPiece:["", "", "", ""]};
+var piece24 = {butt:"", vo:[VO_383, VO_384_M, VO_385_M], nextPiece:["", "", "", ""]};
+
+function newButton(but1, but2, but3, but4) {
+//setting up button1
+  document.getElementById("b1").value = but1.butt;
+  if(but1.butt) {document.getElementById("b1").style.visibility = "visible";}
+  document.getElementById("b1").onclick = function() {
+    document.getElementById("b1").style.visibility = "hidden";
+    document.getElementById("b2").style.visibility = "hidden";
+    document.getElementById("b3").style.visibility = "hidden";
+    document.getElementById("b4").style.visibility = "hidden";
+    myAudioPlay(document.getElementById("myMp3"), document.getElementById("myLrc"), but1.vo, but1);
+    document.getElementById("myMp3").play();
+    myInterval1 = setInterval(function(){
+      if(document.getElementById("myMp3").src.slice(0-but1.vo[but1.vo.length-1].audio.length) == but1.vo[but1.vo.length-1].audio && document.getElementById("myMp3").ended){
+         if(myTally >= 12){
+          myAudioPlay(document.getElementById("myMp3"), document.getElementById("myLrc"), piece23.vo, piece23);
+          document.getElementById("myMp3").play();
+        } else if (myTally % 4 >= 2){
+          myAudioPlay(document.getElementById("myMp3"), document.getElementById("myLrc"), piece24.vo, piece24);
+          document.getElementById("myMp3").play();
+        } else {
+          newButton(eval(but1.nextPiece[0]), eval(but1.nextPiece[1]), eval(but1.nextPiece[2]), eval(but1.nextPiece[3]));
+        }
+        clearInterval(myInterval1);
+      }
+    }, 200);
+  };
+
+//setting up button2
+  document.getElementById("b2").value = but2.butt;
+  if(but2.butt) {document.getElementById("b2").style.visibility = "visible";}
+  document.getElementById("b2").onclick = function() {
+    if(but2.butt == "Call her out.(backtalk)"){
+      document.getElementById("PTally").style.display = "block";
+    }
+    document.getElementById("b1").style.visibility = "hidden";
+    document.getElementById("b2").style.visibility = "hidden";
+    document.getElementById("b3").style.visibility = "hidden";
+    document.getElementById("b4").style.visibility = "hidden";
+    myAudioPlay(document.getElementById("myMp3"), document.getElementById("myLrc"), but2.vo, but2);
+    document.getElementById("myMp3").play();
+    myInterval2 = setInterval(function(){
+      if(document.getElementById("myMp3").src.slice(0-but2.vo[but2.vo.length-1].audio.length) == but2.vo[but2.vo.length-1].audio && document.getElementById("myMp3").ended){
+        if(myTally >= 12){
+          myAudioPlay(document.getElementById("myMp3"), document.getElementById("myLrc"), piece23.vo, piece23);
+          document.getElementById("myMp3").play();
+        } else if (myTally % 4 >= 2){
+          myAudioPlay(document.getElementById("myMp3"), document.getElementById("myLrc"), piece24.vo, piece24);
+          document.getElementById("myMp3").play();
+        } else {
+          newButton(eval(but2.nextPiece[0]), eval(but2.nextPiece[1]), eval(but2.nextPiece[2]), eval(but2.nextPiece[3]));
+        }
+        clearInterval(myInterval2);
+      }
+    }, 200);
+  };
+
+//setting up button3
+  document.getElementById("b3").value = but3.butt;
+  if(but3.butt) {document.getElementById("b3").style.visibility = "visible";}
+  document.getElementById("b3").onclick = function() {
+    document.getElementById("b1").style.visibility = "hidden";
+    document.getElementById("b2").style.visibility = "hidden";
+    document.getElementById("b3").style.visibility = "hidden";
+    document.getElementById("b4").style.visibility = "hidden";
+    myAudioPlay(document.getElementById("myMp3"), document.getElementById("myLrc"), but3.vo, but3);
+    document.getElementById("myMp3").play();
+    myInterval3 = setInterval(function(){
+      if(document.getElementById("myMp3").src.slice(0-but3.vo[but3.vo.length-1].audio.length) == but3.vo[but3.vo.length-1].audio && document.getElementById("myMp3").ended){
+        if(myTally >= 12){
+          myAudioPlay(document.getElementById("myMp3"), document.getElementById("myLrc"), piece23.vo, piece23);
+          document.getElementById("myMp3").play();
+        } else if (myTally % 4 >= 2){
+          myAudioPlay(document.getElementById("myMp3"), document.getElementById("myLrc"), piece24.vo, piece24);
+          document.getElementById("myMp3").play();
+        } else {
+          newButton(eval(but3.nextPiece[0]), eval(but3.nextPiece[1]), eval(but3.nextPiece[2]), eval(but3.nextPiece[3]));
+        }
+        clearInterval(myInterval3);
+      }
+    }, 200);
+  };
+
+//setting up button4
+  document.getElementById("b4").value = but4.butt;
+  if(but4.butt) {document.getElementById("b4").style.visibility = "visible";}
+  document.getElementById("b4").onclick = function() {
+    document.getElementById("b1").style.visibility = "hidden";
+    document.getElementById("b2").style.visibility = "hidden";
+    document.getElementById("b3").style.visibility = "hidden";
+    document.getElementById("b4").style.visibility = "hidden";
+    myAudioPlay(document.getElementById("myMp3"), document.getElementById("myLrc"), but4.vo, but4);
+    document.getElementById("myMp3").play();
+    myInterval4 = setInterval(function(){
+      if(document.getElementById("myMp3").src.slice(0-but4.vo[but4.vo.length-1].audio.length) == but4.vo[but4.vo.length-1].audio && document.getElementById("myMp3").ended){
+        if(myTally >= 12){
+          myAudioPlay(document.getElementById("myMp3"), document.getElementById("myLrc"), piece23.vo, piece23);
+          document.getElementById("myMp3").play();
+        } else if (myTally % 4 >= 2){
+          myAudioPlay(document.getElementById("myMp3"), document.getElementById("myLrc"), piece24.vo, piece24);
+          document.getElementById("myMp3").play();
+        } else {
+          newButton(eval(but4.nextPiece[0]), eval(but4.nextPiece[1]), eval(but4.nextPiece[2]), eval(but4.nextPiece[3]));
+        }
+        clearInterval(myInterval4);
+      }
+    }, 200);
+  };
+}
+
+
+newButton(piece01, piece02, piece03, "");
+
+//play audios continuously
+function myAudioPlay(audioObj, lyricObj, pieces, butt) {
+  var i = 0;
+  var len = pieces.length;
+  audioObj.src = pieces[i].audio;
+  lyricObj.innerHTML = "<b>" + pieces[i].speaker + "</b>: " + pieces[i].lyric;
+  i++;
+  myInterval = setInterval(function(){
+    if(i+1 == len){
+      clearInterval(myInterval);
+    }
+    audioObj.onended = function(){
+      audioObj.src = pieces[i].audio;
+      lyricObj.innerHTML = "<b>" + pieces[i].speaker + "</b>: " + pieces[i].lyric;
+      audioObj.play();
+      i++;
+    }
+  }, 100);
+  myTally += butt.score;
+  switch(myTally){
+    case 0:
+      document.getElementById("PTally").src = "pic/tally0_0.png";
+      break;
+    case 1:
+      document.getElementById("PTally").src = "pic/tally0_1.png";
+      break;
+    case 2:
+      document.getElementById("PTally").src = "pic/tally0_2.png";
+      break;
+    case 4:
+      document.getElementById("PTally").src = "pic/tally1_0.png";
+      break;
+    case 5:
+      document.getElementById("PTally").src = "pic/tally1_1.png";
+      break;
+    case 6:
+      document.getElementById("PTally").src = "pic/tally1_2.png";
+      break;
+    case 8:
+      document.getElementById("PTally").src = "pic/tally2_0.png";
+      break;
+    case 9:
+      document.getElementById("PTally").src = "pic/tally2_1.png";
+      break;
+    case 10:
+      document.getElementById("PTally").src = "pic/tally2_2.png";
+      break;
+    case 12:
+      document.getElementById("PTally").src = "pic/tally3_0.png";
+      break;
+    case 13:
+      document.getElementById("PTally").src = "pic/tally3_1.png";
+      break;
+    default:
+  }
+}
+
+function resetAudio(){
+  myTally = 0;
+  document.getElementById("PTally").style.display = "none";
+  if(myInterval) {clearInterval(myInterval);}
+  if(myInterval1) {clearInterval(myInterval1);}
+  if(myInterval2) {clearInterval(myInterval2);}
+  if(myInterval3) {clearInterval(myInterval3);}
+  if(myInterval4) {clearInterval(myInterval4);}
+  document.getElementById("myMp3").src = "";
+  document.getElementById("myLrc").innerHTML = "";
+  newButton(piece01, piece02, piece03, "");
+}
+
+//document.getElementById("test1").innerHTML = "aaa";
+
+
